aabf0519edc16937875930bfb594daa3bac0a972
mystic-banana <mysticbanana2010@gmail.com>
2025-06-11 14:34:59 +0530

    Deleted check_admin_policy.sh

--- deploy_add_podcast_feed.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Deploying Supabase Edge Function: add-podcast-feed..."
-supabase functions deploy add-podcast-feed --project-ref tbpnsxwldrxdlirxfcor
-echo "Deployment command executed."
